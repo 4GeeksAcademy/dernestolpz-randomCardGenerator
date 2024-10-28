92c28fe003e51476efd15da8fbbc63b688dff120
dernestolpz <dernesto.lopez@gmail.com>
2024-10-28 20:50:35 +0000

    terminado1

--- src/app.js
+++ src/app.js
@@ -2,10 +2,39 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  const suits = ["heart", "diamond", "club", "spade"];
+
+  function generateCard() {
+    const randomSuitIndex = Math.floor(Math.random() * suits.length);
+    const randomValueIndex = Math.floor(Math.random() * values.length);
+
+    const suit = suits[randomSuitIndex];
+    const value = values[randomValueIndex];
+    const symbols = { heart: "♥", diamond: "♦", club: "♣", spade: "♠" };
+
+    document.getElementById("center").innerHTML = value;
+    document.getElementById("top-left").innerHTML = symbols[suit];
+    document.getElementById("bottom-right").innerHTML = symbols[suit];
+
+    const card = document.getElementById("card");
+    card.className = `card ${suit}`;
+  }
+
+  generateCard();
 };
